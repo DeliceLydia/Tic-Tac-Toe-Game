a817b32efad2800ccb543572894114d8b72d5ab4
Delice Lydia <Delydia84@gmail.com>
2020-11-12 12:54:59 +0200

    Add initialize and draw tests

--- spec/tests_spec.rb
+++ spec/tests_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../lib/player'
+require_relative '../lib/board'
 
 describe Player do
   it 'creates a new Player instance' do
@@ -22,4 +23,37 @@ describe Player do
       expect(switched).to eql(current_player)
     end
   end
+end
+
+describe Board do
+    let(:board_game) { Board.new() }
+    let(:combination) {
+        [
+          [1, 2, 3],[4, 5, 6],[7, 8, 9]
+          [1, 4, 7],[2, 5, 8],[3, 6, 9]
+          [1, 5, 9],[7, 5, 3]
+        ] 
+    }
+    describe "#initialize" do
+    it 'create a new board' do
+        expect(board_game.board).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+  end
+    
+  describe "#draw" do
+    it 'should return false if all board elements are numeric' do
+      expect(board_game.draw?).to be(false)
+    end
+
+    it 'should return true when all board elements are not numeric' do
+        board_game.board = ['●','■']
+        expect(board_game.draw?).to be(true)
+    end
+end
+
+    describe "#win" do
+    it 'should return true if any element has a combination of one symbol' do
+        expect(combination.win?).to be(false)
+    end
+end
 end
